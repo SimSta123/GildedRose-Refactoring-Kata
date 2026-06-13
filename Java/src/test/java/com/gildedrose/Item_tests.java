4dcd55ed7f2a6632e861f18e42acf8545afb9ba3
SimSta123 <sstangelberger@gmail.com>
2026-06-13 21:08:17 +0200

    added Item_tests.java with 3 tests

---
+++ Java/src/test/java/com/gildedrose/Item_tests.java
@@ -0,0 +1,48 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Item_tests {
+
+    @Test
+    void nameTest() {
+        Item[] items = new Item[]{new Item("sword", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("sword", app.items[0].name);
+    }
+
+    @Test
+    void qualityLowerThanSellIn() {
+        Item[] items = new Item[]{new Item("sword", 3, 10)};
+        GildedRose app = new GildedRose(items);
+        for(int i = 3; i > 0; i--) {
+            app.updateQuality();
+            //System.out.println("QualityLower: "+ i + ", quality: " + app.items[0].quality);
+        }
+        assertEquals(7, app.items[0].quality);
+    }
+
+    @Test
+    void qualityHigherThanSellIn() {
+        Item[] items = new Item[]{new Item("sword", 3, 2)};
+        GildedRose app = new GildedRose(items);
+        for(int i = 3; i > 0; i--) {
+            app.updateQuality();
+            //System.out.println("Quality higher: " + i + ", quality: " + app.items[0].quality);
+        }
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void qualityEqualToSellIn() {
+        Item[] items = new Item[]{new Item("sword", 3, 3)};
+        GildedRose app = new GildedRose(items);
+        for(int i = 3; i > 0; i--) {
+            app.updateQuality();
+            //System.out.println("Quality higher: " + i + ", quality: " + app.items[0].quality);
+        }
+        assertEquals(0, app.items[0].quality);
+    }
+}
