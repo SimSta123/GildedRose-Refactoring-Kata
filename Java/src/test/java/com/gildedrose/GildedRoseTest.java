df5d0deb29fba95e0788cc0221e00e2649e36ca2
Mike <if23b015@technikum-wien.at>
2026-06-13 21:23:11 +0200

    added normal item boundary behavior tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,28 @@ class GildedRoseTest {
         assertEquals(-1, app.items[0].sellIn);
     }
 
+    @Test
+    void normalItemQualityDegradesTwiceAfterSellDate() {
+        Item[] items = new Item[]{new Item("foo", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void normalItemQualityDoesNotBecomeNegativeAfterSellDate() {
+        Item[] items = new Item[]{new Item("foo", 0, 1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void normalItemNameDoesNotChange() {
+        Item[] items = new Item[]{new Item("foo", 5, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
+
 }
