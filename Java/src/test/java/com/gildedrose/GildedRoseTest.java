2961dc20a9b9bd87559a13c8369a47aabb507f16
Mike <if23b015@technikum-wien.at>
2026-06-13 21:40:09 +0200

    added aged brie quality behavior tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -38,4 +38,28 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void agedBrieQualityIncreases() {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].quality);
+    }
+
+    @Test
+    void agedBrieQualityIncreasesTwiceAfterSellDate() {
+        Item[] items = new Item[]{new Item("Aged Brie", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+    }
+
+    @Test
+    void agedBrieQualityDoesNotExceedFifty() {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
 }
