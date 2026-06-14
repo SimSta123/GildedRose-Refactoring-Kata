8d684e7e2691263d00c23fcfbec7d70c1e753ab2
Tobias <tobias@monyk.at>
2026-06-14 13:56:44 +0200

    - Refactor: Introduce named constants for min/max Quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -6,6 +6,9 @@ class GildedRose {
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -14,24 +17,24 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.equals(AGED_BRIE)
                     && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > 0) {
+                if (item.quality > MIN_QUALITY) {
                     if (!item.name.equals(SULFURAS)) {
                         item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (item.quality < 50) {
+                if (item.quality < MAX_QUALITY) {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals(BACKSTAGE_PASSES)) {
                         if (item.sellIn < 11) {
-                            if (item.quality < 50) {
+                            if (item.quality < MAX_QUALITY) {
                                 item.quality = item.quality + 1;
                             }
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < 50) {
+                            if (item.quality < MAX_QUALITY) {
                                 item.quality = item.quality + 1;
                             }
                         }
@@ -43,10 +46,10 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
+            if (item.sellIn < MIN_QUALITY) {
                 if (!item.name.equals(AGED_BRIE)) {
                     if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0) {
+                        if (item.quality > MIN_QUALITY) {
                             if (!item.name.equals(SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
@@ -55,11 +58,17 @@ class GildedRose {
                         item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (item.quality < 50) {
+                    if (item.quality < MAX_QUALITY) {
                         item.quality = item.quality + 1;
                     }
                 }
             }
+        }
+    }
+
+    private void updateItem(Item item){
+        if(!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)){
+
         }
     }
 }
